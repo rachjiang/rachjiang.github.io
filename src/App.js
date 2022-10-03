1b105b25480409311f5eab0b8112631b0af7d5af
rachelj521 <rjiang521@gmail.com>
2022-10-02 23:14:56 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-brown-png bg-cover">
+      <div className="buttons py-10 mx-auto">
+          <button className="text-white font-bold bg-yellow-700 hover:bg-green-600 py-3 px-8 mx-3 rounded">About Me</button>
+          <button className="text-white font-bold bg-yellow-700 hover:bg-green-600 py-3 px-8 mx-3 rounded">Projects</button>
+          <button className="text-white font-bold bg-yellow-700 hover:bg-green-600 py-3 px-8 mx-3 rounded">Contact</button>
+      </div>
+      <div className="container mx-auto bg-gradient-to-tr from-red-300 via-orange-200 rounded-xl p-8 m-10">
+        <p className="text-2xl">Hi there, I'm</p>
+        <h1 className="text-8xl py-5 font-bold">Rachel Jiang</h1>
+        <p className="text-1xl my-10">enthusiastic problem solver && eager student && empathetic solutions.</p>
+      </div>
     </div>
-  );
+    );
 }
 
 export default App;
