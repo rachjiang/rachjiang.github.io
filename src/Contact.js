ef9be24d56a9c36917eac96ac7c030baea760b57
rachelj521 <rjiang521@gmail.com>
2022-11-12 02:29:28 -0500

    set up flex row with draft of intro, skills, and fun facts and portrait

--- src/Contact.js
+++ src/Contact.js
@@ -12,7 +12,7 @@ function Contact() {
                     <a href="https://www.linkedin.com/in/rachel--jiang/"><LinkedInLogo /></a>
                     <a href="https://github.com/rachelj521"><GithubLogo /></a>
                     <a href="mailto:rjiang521@gmail.com"><MailLogo /></a>
-                    <p className="pt-5">© Rachel Jiang 2022</p>
+                    <p className="pt-8">© Made by Rachel Jiang 2022</p>
                 </div>
         </section>
     )
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,4 @@
 import './App.css';
-// import Navbar from './Navbar';
 
 function Header() {
   return (
