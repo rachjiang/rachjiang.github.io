61cb3ae3cf99fd98397472ca409bef7761f7c781
rachelj521 <rjiang521@gmail.com>
2022-11-12 02:46:49 -0500

    replaced h2 inline tailwind selector rules

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ function About() {
     return(
         <section id="about">
             <div className="container bg-snow">
-                <h2 className="about font-mono text-5xl text-teal font-bold">Get to know me!</h2>
+                <h2>Get to know me!</h2>
                 <img alt="headshot" src={portrait} />
                 <div className="about-container">
                     <p>I'm a full stack developer with a primary focus on the JavaScript PERN stack. As a graduate of The Grace Hopper Program, I've built web and mobile applications by contributing to both front and back ends, with integration through relational and non-relational databases. I enjoy sharing ideas with and abosrbing knowledge from fellow coders when pair-programming. When I program independently, I find deep-satisfaction in creative problem-solving, and feel eager as I find myself in a deep hole of new technologies to test and explore.</p>
