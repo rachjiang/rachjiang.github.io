7517a41311bdbfb2542fef0013d2db4a113f3ebb
rachjiang <rjiang521@gmail.com>
2022-11-15 22:10:57 -0500

    updated github url after username change

--- src/Contact.js
+++ src/Contact.js
@@ -10,7 +10,7 @@ function Contact() {
             <div>
                 <h2 className="text-4xl text-sapphire font-bold pb-10">Let's chat!</h2>
                     <a href="https://www.linkedin.com/in/rachel--jiang/"><LinkedInLogo /></a>
-                    <a href="https://github.com/rachelj521"><GithubLogo /></a>
+                    <a href="https://github.com/rachjiang"><GithubLogo /></a>
                     <a href="mailto:rjiang521@gmail.com"><MailLogo /></a>
                     <p className="pt-8">© 2022 Made by Rachel Jiang</p>
                 </div>
