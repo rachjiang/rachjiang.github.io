8e0e09f4336e6096cf9f7d362a68b6658026b1b1
rachjiang <rjiang521@gmail.com>
2022-12-05 03:58:46 -0500

    header font size for name smaller

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ function Header() {
     <section id="header">
             <h1 className="container">
                 <p className="sm:text-2xl text-3xl font-mono py-10">Hi there, my name is</p>
-                <p className="sm:text-7xl text-8xl font-bold py-10 text-sapphire">Rachel Jiang</p>
+                <p className="sm:text-6xl text-8xl font-bold py-10 text-sapphire">Rachel Jiang</p>
                 <p className="sm:text-2xl text-3xl text-teal font-mono py-10">Full Stack Software Developer</p>     
             </h1>
     </section>
