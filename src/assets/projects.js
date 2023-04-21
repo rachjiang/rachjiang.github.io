e1c7a0e8d5a7ecc4d3da47ccbd23d2d8a59672e9
rachjiang <rjiang521@gmail.com>
2023-04-21 18:54:08 -0400

    added new project

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,4 +1,12 @@
 const projects = [
+{
+   title: "Nifty Gallery",
+   description: "A frontend focused project that allows users to view NFT collections stored on the Ethereum blockchain displayed on a seamless UI.",
+   contribution: "Leveraged Moralis's NFT API with Node and Express to fetch NFTs and their metadata. Implemented a clean, user-friendly UI with React and Bootstrap to display collections as a grid of cards and modals to view more details.",
+   stack: "React, Node.js, Express, Moralis NFT API, Bootstrap 5",
+   image: "NiftyGallery2.png",
+   github: "https://github.com/rachjiang/Nifty-Gallery"
+},
  {
     title: "Tomato",
     description: "Tomato is a new and unique way to connect in real-time with your friends and community. Drop a pin to your precise location with a message, photo, or video to broadcast events to your friends and the world.",
@@ -11,7 +19,7 @@ const projects = [
     title: "Ingredient Inspector",
     description: "A food interpreter which lists possible ingredients and their accuracy rates when the user submits a food-image URL.",
     contribution: "Integrated Clarifai's food-recognition AI model into a react app through Node, set up Express route to upload images for ingredient prediction, created react components for image search and prediction results, styled with Bootstrap.",
-    stack: "React, Express, Node.js, Clarifai, Bootstrap",
+    stack: "React, Express, Node.js, Clarifai, Bootstrap 4",
     image: "II.png",
     github: "https://github.com/rachjiang/Ingredient-Inspector",
  },
@@ -19,7 +27,7 @@ const projects = [
     title: "Pet Play",
     description: "A mock e-commerce website selling pet goodies.",
     contribution: "Loading and viewing cart details for both guests and logged in users, guest checkout page and single product view with add to cart function.",
-    stack: "React, Redux, Express, PostgreSQL, Sequelize, Bootstrap",
+    stack: "React, Redux, Express, PostgreSQL, Sequelize, Bootstrap 4",
     image: "pet-play.png",
     github: "https://github.com/grace-shopper-beetle/pet-play"
  }
